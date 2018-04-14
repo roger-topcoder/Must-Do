47a0b0433faafdf1143c27b44a079ccb1ca01bfa
Harshit Anand <ibm2014034@iiita.ac.in>
2018-04-14 01:44:42 -0700

    first set bit done

---
+++ Bt Magic/First set bit.cpp
@@ -0,0 +1,26 @@
+/* Harshit Anand
+    IIIT Allahabad*/
+#include <bits/stdc++.h>
+using namespace std;
+#define  ll long long
+#define fast_io ios::sync_with_stdio(false);cin.tie(NULL)
+
+unsigned int result(int n){
+    return log2(n&-n) +1;
+}
+int main()
+{
+    fast_io;
+    if(fopen("input.txt", "r"))
+    {
+        freopen("input.txt", "r", stdin);
+        freopen("output1.txt","w",stdout);
+    }
+   int t; cin >> t;
+   while(t--){
+      int n; cin >> n;
+      int p=result(n);
+      cout << p << endl;
+   }   
+   return 0; 
+}
